738d49d91ea09f717787f254cf5714ef5b46e93c
moonyean <wkwk051215@gmail.com>
2026-03-11 16:18:20 +0900

    feat : 모달창

---
+++ frontend/src/components/Card/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const modalCard = ({title, content}) => {
+    return (
+        <dialog>
+            <h2>{title}</h2>
+            <span>{content}</span>
+        <dialog>
+    );
+};
+
